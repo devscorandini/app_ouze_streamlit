613b30594aa0f3100a663770e0f06e567a992526
devscorandini <devscorandini@gmail.com>
2024-08-12 14:37:05 -0300

    Add files via upload

--- pages/2_🔽_Funil_geral.py
+++ pages/2_🔽_Funil_geral.py
@@ -183,6 +183,6 @@ st.markdown(
 
 st.markdown('<div class="centered">', unsafe_allow_html=True)
 
-st.components.html(funnel_2_1_html, height=600, width=1000)
-st.components.html(funnel_2_2_html, height=600, width=1000)
-st.components.html(funnel_3_html, height=600, width=1000)
+st.components.v1.html(funnel_2_1_html, height=600, width=1000)
+st.components.v1.html(funnel_2_2_html, height=600, width=1000)
+st.components.v1.html(funnel_3_html, height=600, width=1000)
